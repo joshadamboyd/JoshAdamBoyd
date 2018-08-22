7c9c831f1ddfc5f16fb34bc9d42f438d6f73a3cd
David Freerksen <dfreerksen@gmail.com>
2018-08-21 21:04:26 -0700

    Category navigation margins

--- wp-content/themes/joshadamboyd/template-parts/post/content-none.php
+++ wp-content/themes/joshadamboyd/template-parts/post/content-none.php
@@ -6,7 +6,7 @@
 
 <section class="no-results not-found">
     <header class="page-header">
-        <h1 class="page-title"><?php _e('Nothing Found', 'joshadamboyd'); ?></h1>
+      <h1 class="page-title"><?php _e('Nothing Found', 'joshadamboyd'); ?></h1>
     </header>
 
     <div class="page-content">
