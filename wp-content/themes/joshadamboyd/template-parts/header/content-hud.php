c8736988ee464d25536cbe62ecee700703f662e1
David Freerksen <dfreerksen@gmail.com>
2018-09-19 21:16:28 -0700

    Hero images

--- wp-content/themes/joshadamboyd/template-parts/header/content-hud.php
+++ wp-content/themes/joshadamboyd/template-parts/header/content-hud.php
@@ -1,31 +1,13 @@
 <section class="hud">
   <div class="jumbotron">
     <div class="container">
-      <h1 class="display-3">Hello, world!</h1>
-
-      <p>
-        This is a template for a simple marketing or informational website. It includes a large callout called a
-        jumbotron and three supporting pieces of content. Use it as a starting point to create something more
-        unique.
-      </p>
-
-      <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
+      <?php if (is_front_page() || joshadamboyd_is_frontpage()) : ?>
+        <?php the_custom_header_markup(); ?>
+      <?php elseif ((is_single() || (is_page() && ! joshadamboyd_is_frontpage())) && has_post_thumbnail(get_queried_object_id())) : ?>
+        <div class="single-featured-image-header">
+          <?php echo get_the_post_thumbnail(get_queried_object_id(), 'joshadamboyd-featured-image'); ?>
+        </div>
+      <?php endif; ?>
     </div>
   </div>
-
-  <!-- <div class="custom-header">
-      <div class="custom-header-media">
-          <?php // the_custom_header_markup(); ?>
-      </div>
-
-        <?php // get_template_part('template-parts/header/site', 'branding'); ?>
-  </div> -->
-
-    <?php
-    // if ((is_single() || (is_page() && ! joshadamboyd_is_frontpage())) && has_post_thumbnail(get_queried_object_id())) :
-    //     echo '<div class="single-featured-image-header">';
-    //     echo get_the_post_thumbnail(get_queried_object_id(), 'joshadamboyd-featured-image');
-    //     echo '</div>';
-    // endif;
-    ?>
 </section>
